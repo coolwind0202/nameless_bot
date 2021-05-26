32ad9d806a3140a8b2e4efb93bf9e98ad4d65999
coolwind0202 <tororo_shidoro@yahoo.co.jp>
2021-05-26 11:52:53 +0900

    asyncio.Queueを利用する実装

--- src/reader.py
+++ src/reader.py
@@ -1,38 +1,50 @@
+import asyncio
+from asyncio.queues import Queue
 import subprocess
 import tempfile
+import dataclasses
 
 import discord
 from discord.ext import commands
+
+@dataclasses.dataclass
+class QueueElement:
+    guild: discord.Guild
+    content: str
 
 class ReaderCog(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
         self.dic_path = "/var/lib/mecab/dic/open-jtalk/naist-jdic/"
         self.model_path = "/usr/share/hts-voice/nitech-jp-atr503-m001/nitech_jp_atr503_m001.htsvoice"
+        self.message_queue = asyncio.Queue()
+        self.bot.loop.create_task(self.worker())
+
+    async def worker(self):
+        while True:
+            await asyncio.sleep(0)
+            element: QueueElement = await self.message_queue.get()
+            voice = QueueElement.guild.voice_client
+
+            if voice is None:
+                continue
+            
+            output =  tempfile.NamedTemporaryFile(suffix=".wav")
+            command = f"open_jtalk -x {self.dic_path} -m {self.model_path} -r 1.0 -ow {output.name}"
+            print(command)
+            proc = subprocess.run(
+                command, shell = True, input=element.content.encode()
+            )
+            output.seek(0)
+
+            source = discord.FFmpegOpusAudio(output.name)
+            voice.play(source, after=lambda e: output.close())
 
     @commands.Cog.listener()
     async def on_message(self, message: discord.Message):
         if message.channel.id != 813598124387860580:
             return
-
-        voice_path = f"{message.channel.id}{message.id}.wav"
-
-        output =  tempfile.NamedTemporaryFile(suffix=".wav")
-        content: str = message.content
-        command = f"open_jtalk -x {self.dic_path} -m {self.model_path} -r 1.0 -ow {output.name}"
-        print(command)
-        proc = subprocess.run(
-            command, shell = True, input=content.encode()
-        )
-
-        output.seek(0)
-
-        source = discord.FFmpegOpusAudio(output.name)
-        if message.guild.voice_client is None:
-            voice: discord.VoiceClient = await message.author.voice.channel.connect()
-        else:
-            voice: discord.VoiceClient = message.guild.voice_client
-        voice.play(source, after=lambda e: output.close())
+        await self.message_queue.put(QueueElement(message.guild, message.content))        
 
 def setup(bot):
     bot.add_cog(ReaderCog(bot))
